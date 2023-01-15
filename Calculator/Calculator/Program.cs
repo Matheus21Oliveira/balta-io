d02e4384991c242e879e2a8bdf2a5df5cfaf1a05
Matheus21Oliveira <matheus21oliveira@gmail.com>
2023-01-15 15:39:54 -0300

    atualização do projeto calculadora

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -3,9 +3,9 @@
 namespace Calculator {
     class Program {
         static void Main(string[] args) {
-          multiplicacao();
+            menu();
         }
-
+             
         static void menu() {
             Console.Clear();
 
@@ -14,10 +14,21 @@ namespace Calculator {
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5- Sair");
 
+            Console.WriteLine("-----------");
             Console.WriteLine("Selecione uma opção: ");
-
             short resultado = short.Parse(Console.ReadLine());
+            
+            switch(resultado)
+            {
+                case 1: soma(); break;
+                case 2: subtracao(); break;
+                case 3: divisao(); break;
+                case 4: multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                defalt: menu(); break;
+            }
         }
         static void soma() {
             Console.Clear();
@@ -30,7 +41,8 @@ namespace Calculator {
 
             float resultado = v1 + v2;
             Console.WriteLine($"O resultado da soma é: {resultado}");
-            
+            Console.ReadKey();
+            menu();
         } 
 
         static void subtracao() {
@@ -44,6 +56,8 @@ namespace Calculator {
 
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
+            menu();
 
         }
 
@@ -58,6 +72,8 @@ namespace Calculator {
 
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão é: {resultado}");
+            Console.ReadKey();
+            menu();
         }
 
         static void multiplicacao() {
@@ -71,6 +87,8 @@ namespace Calculator {
 
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            Console.ReadKey();
+            menu();
         } 
     }
 } 
